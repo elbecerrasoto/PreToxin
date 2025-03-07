5f22ff3d27442cd070f20a33b71fd55c5fb34c3a
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-03-07 11:56:32 -0700

    wip regs2

--- frecuencies_groups.R
+++ frecuencies_groups.R
@@ -1,13 +1,9 @@
+#!/usr/bin/env Rscript
+
 library(tidyverse)
-library(ggthemes)
-library(magrittr)
 library(ggrepel)
 
-get_var_name <- function(var) {
-  deparse(substitute(var))
-}
-
-count_rank <- function(ranks, rank, source) {
+count_rank <- function(ranks, rank, group) {
   Nbacs <- nrow(ranks)
   ranks |>
     group_by(.data[[rank]]) |>
@@ -15,13 +11,13 @@ count_rank <- function(ranks, rank, source) {
     arrange(desc(n)) |>
     rename(name = all_of(rank)) |>
     mutate(
-      source = source,
+      group = group,
       taxon = rank
     )
 }
 
 
-PTTG <- "data/pttg.tsv"
+PTTG <- "PF14449_archs_TaxID_spaces_Groups.tsv"
 ALL_BAC <- "data/all_bac.tsv"
 
 # read ----
@@ -29,16 +25,17 @@ ALL_BAC <- "data/all_bac.tsv"
 pttg <- read_tsv(PTTG)
 all_bac <- read_tsv(ALL_BAC)
 
-taxid_ranks <- all_bac |>
+ranks <- all_bac |>
   distinct(tax_id, .keep_all = TRUE)
 
-
 pttg_ranks <- pttg |>
-  select(-family, -genus) |>
-  left_join(taxid_ranks, join_by(tax_id))
+  left_join(ranks, join_by(tax_id))
+
+pttg_ranks <- pttg_ranks |>
+  rename(group = classification)
 
 
-count_rank(pttg, "family", "all_bac")
+# All PTTGs ----
 
 by_group <- split(pttg_ranks, pttg_ranks$group)
 
@@ -50,68 +47,57 @@ group_freqs_genus <- map(by_group, \(x) count_rank(x, "genus", unique(x$group)))
 
 group_freqs <- bind_rows(group_freqs_fam, group_freqs_genus)
 
-group_freqs |>
-  write_tsv("group_freqs.tsv")
+# Different architectures ----
 
-by_group$`45`
-length(by_group)
+RepeatGroupFreqs <- function(pttg_ranks) {
+  by_group <- split(pttg_ranks, pttg_ranks$group)
 
-pttg
+  group_freqs_fam <- map(by_group, \(x) count_rank(x, "family", unique(x$group))) |>
+    bind_rows()
 
-# wrangle ----
+  group_freqs_genus <- map(by_group, \(x) count_rank(x, "genus", unique(x$group))) |>
+    bind_rows()
 
-# family
+  bind_rows(group_freqs_fam, group_freqs_genus)
+}
 
-family_all$source <- "all_bac"
-family_all$taxon <- "family"
-
-family_pttg <- count_rank(pttg_ranks, family)
-family_pttg$source <- "pttg"
-family_pttg$taxon <- "family"
-
-family <- rbind(family_pttg, family_all)
-family %<>% rename(name = family)
-
-# genus
-
-genus_all <- count_rank(pttg_ranks, genus)
-genus_all$source <- "all_bac"
-genus_all$taxon <- "genus"
-
-genus_pttg <- count_rank(pttg_ranks, genus)
-genus_pttg$source <- "pttg"
-genus_pttg$taxon <- "genus"
-
-genus <- rbind(genus_pttg, genus_all)
-genus %<>% rename(name = genus)
-
-# both
-frecuencies <- rbind(genus, family)
-
-frecuencies |>
-  filter(r >= 0.01)
+pttg_runique <- pttg_ranks |>
+  distinct(arch, .keep_all = TRUE)
 
 
-# plot ----
+group_freqs_unique <- pttg_runique |>
+  RepeatGroupFreqs()
 
-frecuencies |>
-  filter(r > 0.01) |>
-  mutate(name = reorder(name, r)) |>
-  ggplot(aes(label = name, x = name, y = r)) +
-  facet_grid(taxon ~ source) +
-  geom_point() +
-  geom_text_repel() +
-  coord_flip() +
-  theme_fivethirtyeight() +
-  theme(
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
-  ) +
-  labs(
-    title = "Relative Frecuencies bigger than 1% of PT-TG containing genomes",
-    subtitle = "The Relative frecuencies of all NCBI genomes are also shown",
-    caption = "author: Becerra-Soto E."
-  )
 
-ggsave("frecuencies.svg", width = 8.5, height = 11, units = "in")
+group_freqs_unique$source <- "unique_arch"
+group_freqs$source <- "all_pttg"
+
+GroupFreqs <- bind_rows(group_freqs_unique, group_freqs)
+view(GroupFreqs)
+# write_tsv(GroupFreqs, "group_freqs.tsv")
+
+
+# trees ----
+
+# Existence by NCBI genome (not Abundance)
+
+# Existence by Protein (not Abundance)
+
+# Existence by Taxonomy
+
+# Existence by Architecture
+
+
+dim(pttg_ranks)
+pttg_ranks |>
+  distinct(, .keep_all = TRUE)
+
+view(pttg_ranks)
+
+
+pttg_ranks
+
+
+
+
+
